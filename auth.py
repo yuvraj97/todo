759381d6572d1dcef9417be2820c010a6a12e650
Yuvraj Garg <yuvraj97.ml@gmail.com>
2023-02-09 23:02:38 +0530

    update url

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://o15bg4.deta.dev"
 
 
 def signup():
--- todo/http.py
+++ todo/http.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://o15bg4.deta.dev"
 
 
 def fetch_todos(access_token):
